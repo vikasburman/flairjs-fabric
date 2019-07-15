ea4a9e627b1b5af1ab3fa8c4601f4c119b14488f
Vikas Burman <vikasburman@VikasBurmansMBP.fios-router.home>
2019-07-14 20:09:45 -0400

    ...

--- dist/flair.app.js
+++ dist/flair.app.js
@@ -1,12 +1,12 @@
 /**
  * @preserve
- * Flair.js
- * True Object Oriented JavaScript
+ * Flair.js Fabric
+ * Foundation for True Object Oriented JavaScript Apps
  * 
  * Assembly: flair.app
  *     File: ./flair.app.js
- *  Version: 0.0.1
- *  Sun, 14 Jul 2019 23:27:35 GMT
+ *  Version: 0.0.3
+ *  Mon, 15 Jul 2019 00:09:03 GMT
  * 
  * (c) 2017-2019 Vikas Burman
  * MIT
@@ -2666,7 +2666,7 @@
     AppDomain.context.current().currentAssemblyBeingLoaded('');
     
     // register assembly definition object
-    AppDomain.registerAdo('{"name":"flair.app","file":"./flair.app{.min}.js","mainAssembly":"","desc":"True Object Oriented JavaScript","title":"Flair.js","version":"0.0.1","lupdate":"Sun, 14 Jul 2019 23:27:35 GMT","builder":{"name":"flairBuild","version":"1","format":"fasm","formatVersion":"1","contains":["init","func","type","vars","reso","asst","rout","sreg"]},"copyright":"(c) 2017-2019 Vikas Burman","license":"MIT","types":["flair.app.Bootware","flair.app.Handler","flair.ui.vue.VueComponentMembers","flair.api.RestHandler","flair.app.App","flair.app.Host","flair.ui.ViewHandler","flair.ui.Page","flair.app.ClientHost","flair.app.ServerHost","flair.boot.ClientRouter","flair.boot.DIContainer","flair.boot.Middlewares","flair.boot.NodeEnv","flair.boot.ResHeaders","flair.api.RESTfulService","flair.boot.vue.VueSetup","flair.ui.vue.VueView","flair.api.RestInterceptor","flair.ui.ViewInterceptor","flair.ui.ViewState","flair.ui.ViewTransition","flair.app.BootEngine","flair.ui.vue.VueComponent","flair.ui.vue.VueDirective","flair.ui.vue.VueFilter","flair.ui.vue.VueLayout","flair.ui.vue.VueMixin","flair.ui.vue.VuePlugin","flair.boot.ServerRouter"],"resources":[],"assets":[],"routes":[]}');
+    AppDomain.registerAdo('{"name":"flair.app","file":"./flair.app{.min}.js","mainAssembly":"","desc":"Foundation for True Object Oriented JavaScript Apps","title":"Flair.js Fabric","version":"0.0.3","lupdate":"Mon, 15 Jul 2019 00:09:03 GMT","builder":{"name":"flairBuild","version":"1","format":"fasm","formatVersion":"1","contains":["init","func","type","vars","reso","asst","rout","sreg"]},"copyright":"(c) 2017-2019 Vikas Burman","license":"MIT","types":["flair.app.Bootware","flair.app.Handler","flair.ui.vue.VueComponentMembers","flair.api.RestHandler","flair.app.App","flair.app.Host","flair.ui.ViewHandler","flair.ui.Page","flair.app.ClientHost","flair.app.ServerHost","flair.boot.ClientRouter","flair.boot.DIContainer","flair.boot.Middlewares","flair.boot.NodeEnv","flair.boot.ResHeaders","flair.api.RESTfulService","flair.boot.vue.VueSetup","flair.ui.vue.VueView","flair.api.RestInterceptor","flair.ui.ViewInterceptor","flair.ui.ViewState","flair.ui.ViewTransition","flair.app.BootEngine","flair.ui.vue.VueComponent","flair.ui.vue.VueDirective","flair.ui.vue.VueFilter","flair.ui.vue.VueLayout","flair.ui.vue.VueMixin","flair.ui.vue.VuePlugin","flair.boot.ServerRouter"],"resources":[],"assets":[],"routes":[]}');
     
     // assembly load complete
     if (typeof onLoadComplete === 'function') { 
