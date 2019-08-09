5685739ef06285f2290b1985d76beea99d212d81
Vikas Burman <vikasburman@VikasBurmansMBP.fios-router.home>
2019-08-09 14:28:08 -0400

    ...

--- src/flair.app/(root)/cache.js
+++ src/flair.app/(root)/cache.js
@@ -14,16 +14,20 @@ Class('(auto)', Attribute, function() {
         // set cache config
         this.cacheConfig = (typeof cacheConfig === 'number' ? { duration: cacheConfig } : cacheConfig)
         this.enabled = (this.cacheConfig && this.cacheConfig.duration);
+        this.cacheHandler = Port('cacheHandler');
 
         // can be applied
         this.constraints = '(class || struct) && (func && async) && !(timer || on || @fetch || @cache)';
     };
 
-    $$('readOnly');
+    $$('readonly');
     this.cacheConfig = null;
 
     $$('private');
-    this.cacheHandler = Port('cacheHandler');
+    this.cacheHandler = null;
+
+    $$('private');
+    this.enabled = false;
 
     $$('override');
     this.decorateFunction = (base, typeName, memberName, member) => { // eslint-disable-line no-unused-vars
@@ -32,7 +36,7 @@ Class('(auto)', Attribute, function() {
 
         let callMember = async (...args) => {
             let resultData = await member(...args);
-            if (_this.isEnabled) { // save for later
+            if (_this.enabled) { // save for later
                 await _this.cacheHandler.set(cacheId, _this.cacheConfig, resultData);
             }
             return resultData;
@@ -40,7 +44,7 @@ Class('(auto)', Attribute, function() {
 
         // decorated function
         return async function(...args) {
-            if (_this.isEnabled) {
+            if (_this.enabled) {
                 try {
                     return await _this.cacheHandler.get(cacheId, _this.cacheConfig);
                 } catch (err) { // eslint-disable-line no-unused-vars
